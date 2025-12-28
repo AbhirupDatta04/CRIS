90803aa637a509fb0ac72e2b39c484be247b6f4c
Abhirup Datta <66831444+AbhirupDatta04@users.noreply.github.com>
2025-12-28 18:11:49 +0000

    feat: add data-quality filter and risk-aligned metrics to gold layer (CRIS-06)

---
+++ cris/spark/jobs/gold/transactions_gold_account_30d.py
@@ -0,0 +1,85 @@
+"""
+transactions_gold_account_30d.py
+"""
+
+# spend_30d: total customer spending in last 30 days
+# txn_count_30d: frequency of transactions (usage intensity)
+# avg_amount_30d: average ticket size
+# spend_volatility_30d: instability in spending behaviour
+# recency_days: days since last observed transaction
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (
+    col,
+    sum as _sum,
+    count as _count,
+    avg as _avg,
+    stddev as _stddev,
+    countDistinct,
+    max as _max,
+    current_date,
+    date_sub,
+    datediff
+)
+
+SILVER_PATH = "/workspaces/CRIS/cris/datalake/silver/transactions"
+GOLD_PATH = "/workspaces/CRIS/cris/datalake/gold/account_risk_30d"
+
+
+def build_spark():
+    spark = (
+        SparkSession.builder
+        .appName("CRIS-Gold-Account-Risk-30D")
+        .master("local[*]")
+        .getOrCreate()
+    )
+    spark.sparkContext.setLogLevel("WARN")
+    return spark
+
+
+def main():
+    spark = build_spark()
+
+    # 1. Read Silver transactions
+    silver_df = spark.read.parquet(SILVER_PATH)
+
+    # 2. Create event_time from Silver.timestamp
+    silver_df = silver_df.withColumn(
+        "event_time",
+        col("timestamp").cast("timestamp")
+    )
+
+    # 3. Filter last 30 days of activity
+    recent_df = silver_df.filter(
+        col("event_time") >= date_sub(current_date(), 30)
+    )
+    recent_df = recent_df.filter(col("amount") > 0) #Changed as only positive amounts should be there for Risk
+
+
+    # 4. Aggregate into credit risk behavioural signals
+    gold_df = (
+        recent_df
+        .groupBy("account_id")
+        .agg(
+            _sum("amount").alias("spend_30d"),
+            _count("*").alias("txn_count_30d"),
+            _avg("amount").alias("avg_amount_30d"),
+            _stddev("amount").alias("spend_volatility_30d"),
+            countDistinct("merchant_id").alias("merchant_diversity_30d"),
+            datediff(current_date(), _max("event_time")).alias("recency_days")
+        )
+    )
+
+    # 5. Write Gold dataset
+    (
+        gold_df.write
+        .mode("overwrite")
+        .parquet(GOLD_PATH)
+    )
+
+    print("🟡 Gold risk dataset written to:", GOLD_PATH)
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
